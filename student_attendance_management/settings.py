b90c056f7e0c328954ea323d71e646ff4ef7040d
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-03-28 23:16:44 +0600

    Attempting... Changing 'static_root'

--- student_attendance_management/settings.py
+++ student_attendance_management/settings.py
@@ -143,11 +143,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+PROJECT_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_BASE_DIR, 'static'),
+)
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
