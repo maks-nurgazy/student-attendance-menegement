1ed0158f5cd3188a92ff4bed60b89757ad750d10
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-03-30 16:52:10 +0600

    deployed

--- student_attendance_management/settings.py
+++ student_attendance_management/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'nqew7o9-j2(#_o_8)9+l6m$93!rs$p8m*+$^g#fs%r@(ffe_@9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
+
 ALLOWED_HOSTS = ['*', 'https://student-attendance-menegement.herokuapp.com/',
                  'student-attendance-menegement.herokuapp.com/', ]
 
