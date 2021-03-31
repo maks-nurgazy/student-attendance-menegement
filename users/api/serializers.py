65a25072fe38a7e116428c9220e0483dc07cf39d
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-03-31 12:02:50 +0600

    PyJWT==1.7.1 & djangorestframework-simplejwt==4.4.0 & Django==3.0 downgraded for heroku compatibility

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import authenticate
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from university_app.models import Department
