203f065302afe7690992b72d900eee79374487ed
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-05-12 01:25:45 +0600

    asgi file configured again

--- student_attendance_management/asgi.py
+++ student_attendance_management/asgi.py
@@ -1,14 +1,15 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import attendance_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_attendance_management.settings.settings')
+django_asgi_app = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import attendance_app.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             attendance_app.routing.websocket_urlpatterns
