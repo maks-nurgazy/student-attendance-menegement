fdb884773d2d3c994636665fc41707103e52cf2a
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-05-12 00:37:20 +0600

    daphne configured

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn student_attendance_management.wsgi --log-file -
+web: daphne student_attendance_management.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=student_attendance_management.settings.settings -v2
--- student_attendance_management/asgi.py
+++ student_attendance_management/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import attendance_app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_attendance_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_attendance_management.settings.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -15,4 +15,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
