1a4d5b113d909b8422643b5bbad37752e471ddcb
Maksatbek <43789197+maks-nurgazy@users.noreply.github.com>
2021-05-26 16:18:24 +0600

    Cors policy handled|

--- student_attendance_management/settings/settings.py
+++ student_attendance_management/settings/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'course_app',
     'university_app',
     'rest_framework',
+    'corsheaders',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -31,10 +32,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
